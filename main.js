509f0129f19e50d5005e3f684bbd029ad48085aa
guillemomurillo <52514037+guillemomurillo@users.noreply.github.com>
2019-10-31 22:28:22 -0500

    Update and rename main.css to main.js

---
+++ main.js
@@ -0,0 +1,53 @@
+let modal = document.getElementById('modal');
+let flex = document.getElementById('flex');
+let open = document.getElementById('open');
+let close = document.getElementById('close');
+
+open.addEventListener('click', function(){
+    modal.style.display='block';
+})
+
+close.addEventListener('click', function(){
+    modal.style.display='none';
+})
+
+let modal1 = document.getElementById('modal1');
+let flex1 = document.getElementById('flex1');
+let open1= document.getElementById('open1');
+let close1 = document.getElementById('close1');
+
+open1.addEventListener('click', function(){
+    modal1.style.display='block';
+})
+
+close1.addEventListener('click', function(){
+    modal1.style.display='none';
+})
+
+
+let modal2 = document.getElementById('modal2');
+let flex2 = document.getElementById('flex2');
+let open2 = document.getElementById('open2');
+let close2 = document.getElementById('close2');
+
+open2.addEventListener('click', function(){
+    modal2.style.display='block';
+})
+
+close2.addEventListener('click', function(){
+    modal2.style.display='none';
+})
+
+
+let modal3 = document.getElementById('modal3');
+let flex3 = document.getElementById('flex3');
+let open3 = document.getElementById('open3');
+let close3 = document.getElementById('close3');
+
+open3.addEventListener('click', function(){
+    modal3.style.display='block';
+})
+
+close3.addEventListener('click', function(){
+    modal3.style.display='none';
+})
